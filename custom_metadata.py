024ca890550e7766639f14c4c765eede981ce782
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2024-11-13 11:49:19 -0800

    new prompts

--- custom_metadata.py
+++ custom_metadata.py
@@ -1,4 +1,10 @@
-def get_custom_metadata(info, audio):
+import json
 
+# Load the JSON data from the file into a dictionary
+with open('prompts.json', 'r') as f:
+    file_preset_breakdown_dict = json.load(f)
+
+def get_custom_metadata(info, audio):
+    
     # Pass in the relative path of the audio file as the prompt
-    return {"prompt": info["relpath"]}
+    return {"prompt": file_preset_breakdown_dict[info["relpath"]][3]}
