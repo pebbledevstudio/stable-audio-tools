257e3fea0f10821f372a17c466c5631d2a00cf08
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2025-02-27 11:31:21 -0600

    Add utility scripts for model state dict renaming and unwrapping

---
+++ rename_state_dict.py
@@ -0,0 +1,15 @@
+import torch
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model-path", type=str, required=True)
+parser.add_argument("--output-path", type=str, required=True)
+args = parser.parse_args()
+
+model = torch.load(args.model_path)
+
+model["state_dict"] = model["module"]
+model["state_dict"]["diffusion_ema.initted"] = model["state_dict"]["diffusion_ema.initted"].unsqueeze(0)
+model["state_dict"]["diffusion_ema.step"] = model["state_dict"]["diffusion_ema.step"].unsqueeze(0)
+
+torch.save(model, args.output_path)
