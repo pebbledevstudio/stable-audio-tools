2b3548e188ef9879b5ec90dffb99c5a2d50bd4e5
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2025-02-27 11:50:47 -0600

    Update unwrap_model_fix.py to support S3 model loading

--- unwrap_model_fix.py
+++ unwrap_model_fix.py
@@ -4,7 +4,7 @@ import os
 # Run rename_state_dict.py to get the model in the correct format
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model-path", type=str, required=True)
+parser.add_argument("--model-s3-path", type=str, required=True)
 parser.add_argument("--output-path", type=str, required=True)
 parser.add_argument("--model-config", type=str, required=True)
 args = parser.parse_args()
@@ -12,7 +12,8 @@ args = parser.parse_args()
 tmp_path = f"{args.output_path}/{args.model_path}.ckpt"
 file_name = os.path.basename(args.model_path)
 
-os.system(f"python rename_state_dict.py --model-path {args.model_path} --output-path {tmp_path}")
+os.system(f"aws s3 cp {args.model_s3_path} {tmp_path}")
+os.system(f"python rename_state_dict.py --model-path {tmp_path} --output-path {tmp_path}")
 os.system(f"python unwrap_model.py --model-config {args.model_config} --ckpt-path {tmp_path} --name {args.output_path}/{file_name}")
 
 os.remove(tmp_path)
