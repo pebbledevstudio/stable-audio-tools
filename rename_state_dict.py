c5f9cf5f8decf8e93ea5d87d59588553729d4394
Robert Thomas <robert.jeffrey.thomas@gmail.com>
2025-11-04 21:58:17 +0000

    fix rename state dict

--- rename_state_dict.py
+++ rename_state_dict.py
@@ -6,10 +6,10 @@ parser.add_argument("--model-path", type=str, required=True)
 parser.add_argument("--output-path", type=str, required=True)
 args = parser.parse_args()
 
-model = torch.load(args.model_path)
+model = torch.load(args.model_path, map_location='cpu')
 
 model["state_dict"] = model["module"]
-model["state_dict"]["diffusion_ema.initted"] = model["state_dict"]["diffusion_ema.initted"].unsqueeze(0)
-model["state_dict"]["diffusion_ema.step"] = model["state_dict"]["diffusion_ema.step"].unsqueeze(0)
+#model["state_dict"]["diffusion_ema.initted"] = model["state_dict"]["diffusion_ema.initted"].unsqueeze(0)
+#model["state_dict"]["diffusion_ema.step"] = model["state_dict"]["diffusion_ema.step"].unsqueeze(0)
 
 torch.save(model, args.output_path)
